e8aa232a90e0fa42b380ab68a01b97a5d6ffbb17
Christian Mette <mette@pdir.de>
2022-03-28 11:08:11 +0200

    klaro.js and basic klaro-config.js added und tested

---
+++ src/Hooks/GeneratePageHook.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Klaro Consent Manager bundle for Contao Open Source CMS
+ *
+ * Copyright (c) 2022 pdir / digital agentur // pdir GmbH
+ *
+ * @package    krpano-bundle
+ * @link       https://pdir.de/krpano-bundle/
+ * @license    LGPL-3.0-or-later
+ * @author     Mathias Arzberger <develop@pdir.de>
+ * @author     Christian Mette <develop@pdir.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Pdir\ContaoKlaroConsentManager\Hooks;
+
+use Contao\CoreBundle\ServiceAnnotation\Hook;
+use Contao\LayoutModel;
+use Contao\PageModel;
+use Contao\PageRegular;
+
+/**
+ * The generatePage hook is triggered before the main layout (fe_page) is compiled.
+ * It passes the page object, the layout object and a self-reference as arguments
+ * and does not expect a return value.
+ *
+ * @Hook("generatePage")
+ */
+class GeneratePageHook
+{
+    /**
+     * @param PageModel   $pageModel   the current page object
+     * @param LayoutModel $layout      the active page layout applied for rendering the page
+     * @param PageRegular $pageRegular the current page type object
+     */
+    public function __invoke(PageModel $pageModel, LayoutModel $layout, PageRegular $pageRegular): void
+    {
+        $GLOBALS['TL_BODY']['klaro'] = "
+<script defer type='application/javascript' src='bundles/pdircontaoklaroconsentmanager/js/config.js'></script>
+<script
+    defer
+    data-config='klaroConfig'
+    type='application/javascript'
+    src='https://cdn.kiprotect.com/klaro/v0.7/klaro.js'>
+</script>";
+    }
+}
