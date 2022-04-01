08d23e8bf8560e47723e8a16904a1e3ef47483fb
Christian Mette <mette@pdir.de>
2022-04-01 11:38:26 +0200

    tl_klaro_service.php more keys added

--- src/Resources/contao/dca/tl_klaro_service.php
+++ src/Resources/contao/dca/tl_klaro_service.php
@@ -107,6 +107,10 @@ $GLOBALS['TL_DCA']['tl_klaro_service'] = [
         /*
          * Klaro Service Attributes
          */
+        /*
+         *  Each service must have a unique name. Klaro will look for HTML elements with a
+         *  matching 'data-name' attribute to identify elements that belong to this service.
+         */
         'name' => [
             'exclude' => true,
             'inputType' => 'text',
@@ -118,6 +122,10 @@ $GLOBALS['TL_DCA']['tl_klaro_service'] = [
                 'default' => '',
             ],
         ],
+        /*
+         * If 'default' is set to 'true', the service will be enabled by default. This
+         * overrides the global 'default' setting.
+         */
         'default' => [
             'exclude' => true,
             'inputType' => 'checkbox',
@@ -129,5 +137,63 @@ $GLOBALS['TL_DCA']['tl_klaro_service'] = [
                 'default' => '',
             ],
         ],
+        /*
+         * Translations belonging to this service go here. The key `zz` contains default
+         * translations that will be used as a fallback if there are no translations
+         * defined for a given language.
+         */
+        'translations' => [],
+
+        /*
+         * The purpose(s) of this service that will be listed on the consent notice. Do not
+         * forget to add translations for all purposes you list here.
+         */
+        'purposes' => [],
+
+        /*
+         * you an either only provide a cookie name or regular expression (regex) or a list
+         * consisting of a name or regex, a path and a cookie domain. Providing a path and
+         * domain is necessary if you have services that set cookies for a path that is not
+         * "/", or a domain that is not the current domain. If you do not set these values
+         * properly, the cookie can't be deleted by Klaro, as there is no way to access the
+         * path or domain of a cookie in JS. Notice that it is not possible to delete
+         * cookies that were set on a third-party domain, or cookies that have the HTTPOnly
+         * attribute: https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie#new-cookie_domain
+         */
+        'cookies' => [],
+
+        /*
+         * You can define an optional callback function that will be called each time the
+         * consent state for the given service changes. The consent value will be passed as
+         * the first parameter to the function (true=consented). The `service` config will
+         * be passed as the second parameter.
+         */
+        'callback' => [],
+
+        /*
+         * If 'required' is set to 'true', Klaro will not allow this service to be disabled
+         * by the user. Use this for services that are always required for your website to
+         * function (e.g. shopping cart cookies).
+         */
+        'required' => [], // false,
+
+        /*
+         * If 'optOut' is set to 'true', Klaro will load this service even before the user
+         * has given explicit consent. We strongly advise against this.
+         */
+        'optOut' => [], // false,
+
+        /*
+         * If 'onlyOnce' is set to 'true', the service will only be executed once
+         * regardless how often the user toggles it on and off. This is relevant e.g. for
+         * tracking scripts that would generate new page view events every time Klaro
+         * disables and re-enables them due to a consent change by the user.
+         */
+        'onlyOnce' => [], // true,
+
+        /*
+         * unklar
+         */
+        'contextualConsentOnly' => [], // true
     ],
 ];
