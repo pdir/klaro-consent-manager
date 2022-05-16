ea9bc5dc65ed25e9a92ec6f6d8ec7f4b313f496c
Mathias Arzberger <arzberger@pdir.de>
2022-05-16 19:27:36 +0200

    remove configuration

--- src/DependencyInjection/PdirContaoKlaroConsentManagerExtension.php
+++ src/DependencyInjection/PdirContaoKlaroConsentManagerExtension.php
@@ -30,22 +30,10 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 class PdirContaoKlaroConsentManagerExtension extends Extension
 {
     /**
-     * {@inheritdoc}
-     */
-    public function getAlias()
-    {
-        return Configuration::ROOT_KEY;
-    }
-
-    /**
      * @throws \Exception
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $configuration = new Configuration();
-
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new YamlFileLoader(
             $container,
             new FileLocator(__DIR__.'/../Resources/config')
